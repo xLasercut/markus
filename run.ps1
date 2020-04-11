995cb6bb7c0c2531253a67add6852ebba1cf359b
xLasercut <tianxun.wang2004@gmail.com>
2020-04-11 11:46:41 +0100

    removed unused files

--- run.ps1
+++
@@ -1,8 +0,0 @@
-$envVars = Get-Content "./config.env"
-
-ForEach ($envVar in $envVars) {
-  $key, $val = $envVar.split("=")
-  Set-Item "env:$key" $val
-}
-
-npm run serve
--- run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-while read LINE; do export "$LINE"; done < ./config.env
-
-npm run serve
