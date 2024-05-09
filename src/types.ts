1fa35f2e7506fed2d0182232aa2e0079ad09af7e
Tim Wang <tianxun.wang1@nhs.net>
2024-05-09 10:49:15 +0100

    fixed build

--- src/types.ts
+++ src/types.ts
@@ -1,10 +1,12 @@
-import { SlashCommandBuilder } from 'discord.js';
+import { SlashCommandBuilder, SlashCommandOptionsOnlyBuilder } from 'discord.js';
 import { z } from 'zod';
 import { Item, UserItem } from './models/api';
 import { POST_TYPES } from './constants';
 import { Config } from './models/config';
 
-type TDiscordCommand = Omit<SlashCommandBuilder, 'addSubcommand' | 'addSubcommandGroup'>;
+type TDiscordCommand =
+  | Omit<SlashCommandBuilder, 'addSubcommand' | 'addSubcommandGroup'>
+  | SlashCommandOptionsOnlyBuilder;
 type TItem = z.infer<typeof Item>;
 type TUserItem = z.infer<typeof UserItem>;
 type TPost = (typeof POST_TYPES)[keyof typeof POST_TYPES];
